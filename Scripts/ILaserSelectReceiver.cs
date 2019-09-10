2f6a39ba30ea89c98e30132bb20f890bff6e8ed3
Inspiron5980 <puyu986@fuwamofu.com>
2019-09-10 15:02:43 +0900

    [update]レーザーポインターの選択を受付するサンプル処理を実装

--- Scripts/ILaserSelectReceiver.cs
+++ Scripts/ILaserSelectReceiver.cs
@@ -1,4 +1,7 @@
 ﻿
+/// <summary>
+/// レーザーポインタからの操作を受け取るインターフェイス
+/// </summary>
 public interface ILaserSelectReceiver
 {
     void LaserSelectReceiver();
--- Scripts/VRUI.cs
+++ Scripts/VRUI.cs
@@ -1,11 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// レーザーポインターの選択受付サンプルクラス
+/// </summary>
 public class VRUI : MonoBehaviour, ILaserSelectReceiver
 {
+    float TRANSLUCENT = 0.5f;
+    float OPACITY = 1f;
+    Material material;
+    bool isTranslucent;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    /// <summary>
+    /// VRUI初期化
+    /// </summary>
+    public void Initialize()
+    {
+        material = this.GetComponent<MeshRenderer>().material;
+        changeTransparency(true);
+    }
+
+    /// <summary>
+    /// レーザーポインター選択受付
+    /// </summary>
     public void LaserSelectReceiver()
     {
-        Debug.Log("test");
+        changeTransparency(!isTranslucent);
+    }
+
+    void changeTransparency(bool doMakeTranslucent)
+    {
+        var color = material.color;
+        if (doMakeTranslucent)
+        {
+            material.color = new Color(color.r, color.g, color.b, TRANSLUCENT);
+        }
+        else
+        {
+            material.color = new Color(color.r, color.g, color.b, OPACITY);
+        }
+
+        isTranslucent = doMakeTranslucent;
     }
 }
