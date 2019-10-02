54c009da409ade98fba3bd7a0674e40f1f38a3b9
Inspiron5980 <puyu986@fuwamofu.com>
2019-10-02 11:56:41 +0900

    [update]レーザーからの受取インターフェイスクラスの名前を変更

--- Scripts/ILaserSelectReceiver.cs
+++
@@ -1,8 +0,0 @@
-﻿
-/// <summary>
-/// レーザーポインタからの操作を受け取るインターフェイス
-/// </summary>
-public interface ILaserSelectReceiver
-{
-    void LaserSelectReceiver();
-}
--- Scripts/VRUI.cs
+++ Scripts/VRUI.cs
@@ -1,49 +1,8 @@
-﻿using UnityEngine;
-
+﻿
 /// <summary>
-/// レーザーポインターの選択受付サンプルクラス
+/// レーザーポインタからの操作を受け取るインターフェイス
 /// </summary>
-public class VRUI : MonoBehaviour, ILaserSelectReceiver
+public interface VRUI
 {
-    float TRANSLUCENT = 0.5f;
-    float OPACITY = 1f;
-    Material material;
-    bool isTranslucent;
-
-    void Start()
-    {
-        Initialize();
-    }
-
-    /// <summary>
-    /// VRUI初期化
-    /// </summary>
-    public void Initialize()
-    {
-        material = this.GetComponent<MeshRenderer>().material;
-        changeTransparency(true);
-    }
-
-    /// <summary>
-    /// レーザーポインター選択受付
-    /// </summary>
-    public void LaserSelectReceiver()
-    {
-        changeTransparency(!isTranslucent);
-    }
-
-    void changeTransparency(bool doMakeTranslucent)
-    {
-        var color = material.color;
-        if (doMakeTranslucent)
-        {
-            material.color = new Color(color.r, color.g, color.b, TRANSLUCENT);
-        }
-        else
-        {
-            material.color = new Color(color.r, color.g, color.b, OPACITY);
-        }
-
-        isTranslucent = doMakeTranslucent;
-    }
+    void Receiver();
 }
